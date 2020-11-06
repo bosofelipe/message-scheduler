09b4e38e9a8d750c5aad0771dfe16c8efc020a5e
felipeboso <felipe.boso@simpluste.com.br>
2020-11-06 11:22:08 -0300

    MOD - Ajuste retorno para resource de listagem de agendamentos

--- src/main/java/com/luizalabs/controller/MessageController.java
+++ src/main/java/com/luizalabs/controller/MessageController.java
@@ -1,7 +1,6 @@
 package com.luizalabs.controller;
 
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,7 +9,6 @@ import com.luizalabs.ApiPageable;
 import com.luizalabs.domain.Message;
 
 import com.luizalabs.dto.MessageDTO;
-import com.luizalabs.dto.Messages;
 import com.luizalabs.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
